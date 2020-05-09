47b3058c1fae87a4108c1eb2db9ff33817cd2a4e
Vineeta Pathak <pathak.vineeta786@gmail.com>
2020-05-10 02:32:46 +0530

    updated code

---
+++ Mathematics/GP Term.py
@@ -0,0 +1,48 @@
+#{ 
+#Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+ # } Driver Code Ends
+
+#User function Template for python3
+import math
+#Compelte his function
+def termOfGP(A,B,N):
+    #Your code here
+    c = B/A
+    ans = (A*(math.pow(c,N-1)))
+    
+    return math.floor(ans)
+    
+
+
+
+#{ 
+#Driver Code Starts.
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        AB=[int(x) for x in input().strip().split()]
+        A=AB[0]
+        B=AB[1]
+        
+        N=int(input())
+        
+        print(math.floor(termOfGP(A,B,N)))
+        
+        T-=1
+    
+    
+
+
+
+
+if __name__=="__main__":
+    main()
+#} Driver Code Ends
